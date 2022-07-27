57702ab118e5cd89cb4b45312a95f44f774efffd
Archana Shinde <vyawahare.archana@gmail.com>
2022-07-27 16:00:10 -0400

    css added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+app.use('', express.static(path.join(__dirname, '/../public')))
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "..public/index.html"));
 });
+
+
+
 const port = process.env.PORT || 4006;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
