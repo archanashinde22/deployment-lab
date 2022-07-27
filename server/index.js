1d6e800921ffe4cf912f9e48a09cd538f98aef9f
Archana Shinde <vyawahare.archana@gmail.com>
2022-07-27 14:48:47 -0400

    deployment lab

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+const port = process.env.PORT || 4006;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
