31ddeff97aaec3c54da7ed4af97cd375fed6dd42
Archana Shinde <vyawahare.archana@gmail.com>
2022-07-28 15:30:02 -0400

    added rollbar

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.use(express.static(path.join(__dirname, "/../public")));
 app.get("/", function (req, res) {
+  rollbar.info("Main page accessed")
   res.sendFile(path.join(__dirname, "..public/index.html"));
 });
 
